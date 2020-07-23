1a536a5dc782d5a5953f0834563837bc50cdfe4a
Carlos Ramirez <charlydeveloping@gmail.com>
2020-07-23 17:26:24 -0400

    CRUD Base

---
+++ lib/DataBase/CRUD.dart
@@ -0,0 +1,31 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter_with_sqlite/DataBase/DB.dart';
+
+abstract class CRUD {
+  final String table;
+  const CRUD(this.table)
+
+  Future<Database>get database async {
+    return await DB().open();
+  }
+
+  query(String sql, List<dynamic> arguments) async {
+	final db = await database;
+    return await db.rawQuery(sql, arguments);
+  }
+
+  update(Map<String, dynamic> data) async {
+    final db = await database;
+    return await db.update(table, data, where: "id=?", whereArgs: [data["id"]]);
+  }
+
+  insert(Map<String, dynamic> data) async {
+    final db = await database;
+    return await db.insert(table, data);
+  }
+
+  delete(int id) async {
+    final db = await database;
+    return await db.delete(table, where: "id=?", whereArgs: [id]);
+  }
+}
